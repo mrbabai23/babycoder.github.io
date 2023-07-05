0ed07e684e5347d58d7face8114b6e45275e7674
mrbabai23 <mrsarkar1923@gmail.com>
2023-07-05 16:58:44 +0530

    Update

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function submitForm() {
     document.form.submit();
     document.form.reset();
-    }
+}
+
+var loader = document.getElementById("loader");
+
+window.addEventListener("load", function(){
+    loader.style.display="none";
+})
